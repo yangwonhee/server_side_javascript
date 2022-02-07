fadfaeadef9d5f633956db7753fdbb8e7e6a796e
yangwonhee <yangwonhee@kakao.com>
2022-02-07 23:57:30 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+app.get('/', function(req, res){
+    res.send('Hello home page');
+});
+
+app.get('/login', function(req, res){
+    res.send('login please');
+})
+
+app.listen(3000, function(){
+    console.log('Connected 3000 port!')
+});
---
+++ webserver2.js
@@ -0,0 +1,26 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+
+// same with upper code
+const server = http.createServer(function(req, res){
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World!');
+});
+
+server.listen(port, hostname, function(){
+    console.log(`Server running at http://${hostname}:${port}/`)
+});
